d41ba70dd16e2210686ce5cc15a4fd0ae6e28e8e
Anush <93262028+Anush019@users.noreply.github.com>
2024-07-06 19:30:04 +0530

    Add files via upload

---
+++ Anush.java
@@ -0,0 +1,12 @@
+class Anush {
+    static int a=10; 
+    static int a=100; 
+   
+
+    public static void main(String[] args) {
+        System.out.println('2'+10);  
+        
+        
+    }
+}
+ 
---
+++ Sample.java
@@ -0,0 +1,9 @@
+class Sample
+{
+    public static void main(String[]ags)
+    { 
+        for(i=1;i<10;i++){
+        system.out.println('anush')
+        }
+    }
+}
